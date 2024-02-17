c3116427f039557a9ac71f2137ada298ed10a72a
Eduardo De Souza Santos <119471845+Eduardoss45@users.noreply.github.com>
2024-02-17 19:01:57 -0300

    Desafio 1 comentado e explicado

--- Modulo-1/Desafio-1/index.js
+++ Modulo-1/Desafio-1/index.js
@@ -1,40 +1,50 @@
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
+    // Seleciona os elementos do DOM e define variáveis de estado
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
     this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
-    this.handleCilck = this.handleCilck.bind(this);
+    this.activeClass = "active"; // Classe usada para indicar estado ativo
+    this.handleCilck = this.handleCilck.bind(this); // Bind do método para manter o contexto correto
   }
 
+  // Método para animar os links da navegação
   animateLinks() {
-    this.navLinks.forEach((link, index) =>{
-        link.style.animation
-        ? (link.style.animation = "") : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`)
-    })
+    this.navLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
+    });
   }
 
+  // Método para lidar com o clique no ícone do menu
   handleCilck() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass)
-    this.animateLinks();
+    this.navList.classList.toggle(this.activeClass); // Adiciona/Remove a classe ativa à lista de navegação
+    this.mobileMenu.classList.toggle(this.activeClass); // Adiciona/Remove a classe ativa ao ícone do menu
+    this.animateLinks(); // Chama o método para animar os links
   }
 
+  // Método para adicionar evento de clique ao ícone do menu
   addClickEvent() {
     this.mobileMenu.addEventListener("click", this.handleCilck);
   }
 
+  // Método de inicialização
   init() {
     if (this.mobileMenu) {
-      this.addClickEvent();
+      this.addClickEvent(); // Se o ícone do menu existir, adiciona o evento de clique
     }
     return this;
   }
 }
 
+// Instanciação da classe MobileNavbar com os seletores dos elementos do DOM
 const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li"
+  ".mobile-menu", // Seletor do ícone do menu
+  ".nav-list", // Seletor da lista de navegação
+  ".nav-list li" // Seletor dos itens da lista de navegação
 );
-mobileNavbar.init();
+
+mobileNavbar.init(); // Inicializa a funcionalidade do menu
